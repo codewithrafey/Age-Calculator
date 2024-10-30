11d7e6ce7d7aa052e734d175ce23199e62c4b958
codewithrafey <rafeyrashid0@gmail.com>
2024-10-30 22:40:06 +0500

    age-calculator

---
+++ app.js
@@ -0,0 +1,20 @@
+function sub(){
+    var selectedDate = document.getElementById("dateSelect").value;
+    var selectedMonth = document.getElementById("monthSelect").value;
+    var selectedYear = document.getElementById("yearSelect").value;
+    if (selectedDate === "Choose..." || selectedMonth === "Choose..." || selectedYear === "Choose...") {
+        document.getElementById('res').innerHTML = 'Result'
+        document.getElementById("output").innerHTML = 'Invalid date. Please select all fields.';
+        return; // Exit the function
+    }
+ 
+
+var dob = new Date(selectedYear, selectedMonth, selectedDate);
+var today = new Date();
+
+var minus = today - dob;
+var formula = minus / (1000 * 60 * 60 * 24 * 365);
+        document.getElementById('res').innerHTML = 'Result'
+        document.getElementById("output").innerHTML = 'Your age is ' + Math.floor(formula) + ' year';
+
+}
