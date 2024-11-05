8709b689a98ba31352905e5dceeebbea6e7be72e
codewithrafey <rafeyrashid0@gmail.com>
2024-11-05 17:17:22 +0500

    calculate_age

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ function sub() {
     ageYears--;
     ageMonths += 12;
   }
+  if(dob > today){
+    document.getElementById("res").innerHTML = "Result";
+    document.getElementById("output").style.color = "red";
+    document.getElementById("output").innerHTML =
+      "Date of birth needs to be earlier than the age at date.";
+    return;
+
+  }
+
   document.getElementById("res").innerHTML = "Result";
   document.getElementById("output").style.color = "black";
   document.getElementById(
